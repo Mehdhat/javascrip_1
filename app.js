91c75fb6ea690aff3481504115deb999deded935
Mehdhat <137874716+Mehdhat@users.noreply.github.com>
2023-10-05 07:44:24 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+let a=10;//I am assigning 10 to variable a
+console.log(a)//To print 10
+/*This lines will return the value which i have stored in a*/
+
+console.log("Mehdhat")
+/*Printing my name*/
